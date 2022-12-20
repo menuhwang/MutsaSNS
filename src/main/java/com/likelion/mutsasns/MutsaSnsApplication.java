66cb6173985dd72466411c3e26decf6f52fe9c7f
menuHwang <hmin0206@naver.com>
2022-12-20 14:27:47 +0900

    [Feat] Auditing 설정

--- src/main/java/com/likelion/mutsasns/MutsaSnsApplication.java
+++ src/main/java/com/likelion/mutsasns/MutsaSnsApplication.java
@@ -2,8 +2,10 @@ package com.likelion.mutsasns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MutsaSnsApplication {
 
     public static void main(String[] args) {
