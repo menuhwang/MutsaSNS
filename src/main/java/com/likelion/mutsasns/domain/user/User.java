b45a5747f602e67277716e799380735de757a319
menuHwang <hmin0206@naver.com>
2022-12-20 14:26:05 +0900

    [Feat] User Entity 작성
    - enum 타입의 유저 권한 UserRole 작성

---
+++ src/main/java/com/likelion/mutsasns/domain/user/User.java
@@ -0,0 +1,96 @@
+package com.likelion.mutsasns.domain.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated
+    @Builder.Default
+    @Column(nullable = false)
+    private Role role = Role.USER;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private boolean enabled = true;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdDateTime;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedDateTime;
+
+    private LocalDateTime deletedDateTime;
+
+    @Builder
+    public User(Long id, String username, String password, Role role, boolean enabled) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.enabled = enabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<GrantedAuthority>(List.of(new SimpleGrantedAuthority(role.name())));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
