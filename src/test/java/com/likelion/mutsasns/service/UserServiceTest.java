96a514333e5212dcd2efe403a1171bd58b388067
menuHwang <hmin0206@naver.com>
2022-12-20 15:45:57 +0900

    [Test] UserService 회원가입 구현

--- src/test/java/com/likelion/mutsasns/service/UserServiceTest.java
+++ src/test/java/com/likelion/mutsasns/service/UserServiceTest.java
@@ -15,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class UserServiceTest {
     private final PasswordEncoder passwordEncoder = Mockito.mock(PasswordEncoder.class);
@@ -23,22 +24,41 @@ class UserServiceTest {
     private final UserService userService = new UserService(passwordEncoder, jwtProvider, userRepository);
 
     private final String MOCK_TOKEN = "mockJwtToken";
+    private final Long USER_ID = 1L;
     private final String USERNAME = "tester";
     private final String PASSWORD = "password";
     private final User USER = User.builder()
+            .id(USER_ID)
             .username(USERNAME)
             .password(PASSWORD)
             .build();
-    private final LoginRequest loginRequest = new LoginRequest(USERNAME, PASSWORD);
+    private final LoginRequest LOGIN_REQUEST = new LoginRequest(USERNAME, PASSWORD);
+    private final JoinRequest JOIN_REQUEST = new JoinRequest(USERNAME, PASSWORD);
 
     @Test
     void login() {
         given(jwtProvider.generateToken(any(User.class))).willReturn(MOCK_TOKEN);
         given(userRepository.findByUsername(USERNAME)).willReturn(Optional.of(USER));
-        LoginResponse loginResponse = userService.login(loginRequest);
+        LoginResponse loginResponse = userService.login(LOGIN_REQUEST);
 
         assertEquals(MOCK_TOKEN, loginResponse.getJwt());
         verify(jwtProvider).generateToken(any(User.class));
         verify(userRepository).findByUsername(USERNAME);
+    }
+
+    @Test
+    void join() {
+        given(userRepository.existsByUsername(USERNAME)).willReturn(false);
+        given(passwordEncoder.encode(PASSWORD)).willReturn(PASSWORD);
+        when(userRepository.save(any(User.class))).thenReturn(USER);
+
+        JoinResponse result = userService.join(JOIN_REQUEST);
+
+        assertEquals(USER_ID, result.getId());
+        assertEquals(USERNAME, result.getUserName());
+
+        verify(userRepository).existsByUsername(USERNAME);
+        verify(passwordEncoder).encode(PASSWORD);
+        verify(userRepository).save(any(User.class));
     }
 }
