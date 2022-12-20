55c0b7f681a5dc29f59eb372a1fecac2b554f215
menuHwang <hmin0206@naver.com>
2022-12-20 14:54:08 +0900

    [Test] 로그인 테스트 수정

--- src/test/java/com/likelion/mutsasns/service/UserServiceTest.java
+++ src/test/java/com/likelion/mutsasns/service/UserServiceTest.java
@@ -1,11 +1,15 @@
 package com.likelion.mutsasns.service;
 
 import com.likelion.mutsasns.domain.user.User;
+import com.likelion.mutsasns.dto.user.LoginRequest;
+import com.likelion.mutsasns.dto.user.LoginResponse;
 import com.likelion.mutsasns.repository.UserRepository;
 import com.likelion.mutsasns.security.provider.JwtProvider;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -21,16 +25,20 @@ class UserServiceTest {
     private final String MOCK_TOKEN = "mockJwtToken";
     private final String USERNAME = "tester";
     private final String PASSWORD = "password";
+    private final User USER = User.builder()
+            .username(USERNAME)
+            .password(PASSWORD)
+            .build();
     private final LoginRequest loginRequest = new LoginRequest(USERNAME, PASSWORD);
 
     @Test
     void login() {
-        given(passwordEncoder.encode(PASSWORD)).willReturn(PASSWORD);
         given(jwtProvider.generateToken(any(User.class))).willReturn(MOCK_TOKEN);
+        given(userRepository.findByUsername(USERNAME)).willReturn(Optional.of(USER));
         LoginResponse loginResponse = userService.login(loginRequest);
 
         assertEquals(MOCK_TOKEN, loginResponse.getJwt());
-        verify(passwordEncoder).encode(PASSWORD);
         verify(jwtProvider).generateToken(any(User.class));
+        verify(userRepository).findByUsername(USERNAME);
     }
 }
