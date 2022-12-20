01cc6175de0a9fa89cc40a4b297c730722cf6ea9
menuHwang <hmin0206@naver.com>
2022-12-20 17:27:55 +0900

    [Feat] UserController 회원가입 구현

--- src/main/java/com/likelion/mutsasns/controller/UserController.java
+++ src/main/java/com/likelion/mutsasns/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.likelion.mutsasns.controller;
 
+import com.likelion.mutsasns.dto.SuccessResponse;
+import com.likelion.mutsasns.dto.user.JoinRequest;
+import com.likelion.mutsasns.dto.user.JoinResponse;
 import com.likelion.mutsasns.dto.user.LoginRequest;
 import com.likelion.mutsasns.dto.user.LoginResponse;
 import com.likelion.mutsasns.service.UserService;
@@ -15,8 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("login")
+    @PostMapping("/login")
     public LoginResponse login(@RequestBody LoginRequest loginRequest) {
         return userService.login(loginRequest);
+    }
+
+    @PostMapping("/join")
+    public SuccessResponse<JoinResponse> join(@RequestBody JoinRequest joinRequest) {
+        JoinResponse response = userService.join(joinRequest);
+        return new SuccessResponse<>(response);
     }
 }
