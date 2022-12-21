7556a9c4732b2030d52eaf67e50139083518935b
menuHwang <hmin0206@naver.com>
2022-12-21 10:26:43 +0900

    [Feat] Post DTO 작성

---
+++ src/main/java/com/likelion/mutsasns/dto/post/PostRequest.java
@@ -0,0 +1,27 @@
+package com.likelion.mutsasns.dto.post;
+
+import com.likelion.mutsasns.domain.post.Post;
+import com.likelion.mutsasns.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class PostRequest {
+    private String title;
+    private String body;
+
+    private PostRequest() {
+    }
+
+    public PostRequest(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public Post toEntity(User user) {
+        return Post.builder()
+                .title(title)
+                .body(body)
+                .user(user)
+                .build();
+    }
+}
