a9417e7a7747e09bec066e299444817fe1f20a32
menuHwang <hmin0206@naver.com>
2022-12-21 11:47:17 +0900

    [Feat] PostController 포스트 작성 구현

---
+++ src/main/java/com/likelion/mutsasns/controller/PostController.java
@@ -0,0 +1,30 @@
+package com.likelion.mutsasns.controller;
+
+import com.likelion.mutsasns.dto.SuccessResponse;
+import com.likelion.mutsasns.dto.post.PostRequest;
+import com.likelion.mutsasns.dto.post.PostResponse;
+import com.likelion.mutsasns.dto.post.PostResponseWrapper;
+import com.likelion.mutsasns.service.PostService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/posts")
+@RequiredArgsConstructor
+public class PostController {
+    private final PostService postService;
+
+    @PostMapping("")
+    public SuccessResponse<PostResponseWrapper> create(Principal principal, @RequestBody PostRequest postRequest) {
+        log.info("PostRequest title:{}, body:{}", postRequest.getTitle(), postRequest.getBody());
+        PostResponse response = postService.create(principal, postRequest);
+        return new SuccessResponse<>(response.toWrapperDTO("포스트 등록 완료"));
+    }
+}
