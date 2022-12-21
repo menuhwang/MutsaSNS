aea25e3a66ddb546a6ff240ff80545259f10dded
menuHwang <hmin0206@naver.com>
2022-12-21 18:29:31 +0900

    [Feat] PostNotFoundException 추가

--- src/main/java/com/likelion/mutsasns/exception/ErrorCode.java
+++ src/main/java/com/likelion/mutsasns/exception/ErrorCode.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 포스트가 없습니다."),
     DUPLICATED_USERNAME(HttpStatus.CONFLICT, "UserName이 중복됩니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "패스워드가 잘못되었습니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
