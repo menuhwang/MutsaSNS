b5a73b9658045ad14165ddd0c205d63478d98b29
menuHwang <hmin0206@naver.com>
2022-12-23 09:02:40 +0900

    KeyWord 수정

--- src/main/java/com/likelion/mutsasns/controller/HelloController.java
+++ src/main/java/com/likelion/mutsasns/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/hello")
 public class HelloController {
-    private final String KEYWORD = "darkchocolate";
+    private final String KEYWORD = "popin";
     @GetMapping("")
     public String hello() {
         return KEYWORD;
