5dc79e0d1fd90477d7d19bddf3b78388ff813868
menuHwang <hmin0206@naver.com>
2022-12-25 16:10:24 +0900

    키워드 변경

--- src/main/java/com/likelion/mutsasns/controller/HelloController.java
+++ src/main/java/com/likelion/mutsasns/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/hello")
 public class HelloController {
-    private final String KEYWORD = "popin";
+    private final String KEYWORD = "nivea";
     @GetMapping("")
     public String hello() {
         return KEYWORD;
