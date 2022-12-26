0848a44fc43b0ead0647271a806369ca57c57a79
menuHwang <hmin0206@naver.com>
2022-12-26 10:21:55 +0900

    [Refactor] SuccessResponse, ErrorResponse 리팩토링

--- src/main/java/com/likelion/mutsasns/controller/UserController.java
+++ src/main/java/com/likelion/mutsasns/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.likelion.mutsasns.controller;
 
-import com.likelion.mutsasns.dto.SuccessResponse;
+import com.likelion.mutsasns.dto.ResultResponse;
 import com.likelion.mutsasns.dto.user.*;
 import com.likelion.mutsasns.service.UserService;
 import com.likelion.mutsasns.support.annotation.Login;
@@ -19,24 +19,24 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/login")
-    public SuccessResponse<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+    public ResultResponse<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
         log.info("로그인 id : {}", loginRequest.getUserName());
         LoginResponse response = userService.login(loginRequest);
-        return new SuccessResponse<>(response);
+        return ResultResponse.success(response);
     }
 
     @PostMapping("/join")
-    public SuccessResponse<JoinResponse> join(@RequestBody JoinRequest joinRequest) {
+    public ResultResponse<JoinResponse> join(@RequestBody JoinRequest joinRequest) {
         log.info("회원가입 id : {}", joinRequest.getUserName());
         JoinResponse response = userService.join(joinRequest);
-        return new SuccessResponse<>(response);
+        return ResultResponse.success(response);
     }
 
     @Login
     @PostMapping("/{id}/role/change")
-    public SuccessResponse<UserDetailResponse> updateUserRole(@ApiIgnore Principal principal, @PathVariable Long id, @RequestBody UpdateUserRoleRequest updateUserRoleRequest) {
+    public ResultResponse<UserDetailResponse> updateUserRole(@ApiIgnore Principal principal, @PathVariable Long id, @RequestBody UpdateUserRoleRequest updateUserRoleRequest) {
         log.info("유저 권한 변경 userId:{}", id);
         UserDetailResponse response = userService.updateRole(principal.getName(), id, updateUserRoleRequest);
-        return new SuccessResponse<>(response);
+        return ResultResponse.success(response);
     }
 }
---
+++ src/main/java/com/likelion/mutsasns/dto/ResultResponse.java
@@ -0,0 +1,23 @@
+package com.likelion.mutsasns.dto;
+
+import com.likelion.mutsasns.exception.AbstractBaseException;
+import lombok.Getter;
+
+@Getter
+public class ResultResponse<T> {
+    private final String resultCode;
+    private final T result;
+
+    private ResultResponse(String resultCode, T result) {
+        this.resultCode = resultCode;
+        this.result = result;
+    }
+
+    public static  <T> ResultResponse<T> success(T result) {
+        return new ResultResponse<>("SUCCESS", result);
+    }
+
+    public static ResultResponse<ErrorResponse> error(AbstractBaseException e) {
+        return new ResultResponse<>("ERROR", e.errorResponse());
+    }
+}
