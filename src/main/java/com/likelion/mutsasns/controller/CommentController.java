4842928b2cb94bae642f9931db3d4db7baf58f9d
menuHwang <hmin0206@naver.com>
2023-01-03 15:06:02 +0900

    [Add] Swagger Authorize 설정 Login 어노테이션 추가

--- src/main/java/com/likelion/mutsasns/controller/CommentController.java
+++ src/main/java/com/likelion/mutsasns/controller/CommentController.java
@@ -4,6 +4,7 @@ import com.likelion.mutsasns.dto.ResultResponse;
 import com.likelion.mutsasns.dto.comment.CommentDetailResponse;
 import com.likelion.mutsasns.dto.comment.CommentRequest;
 import com.likelion.mutsasns.service.CommentService;
+import com.likelion.mutsasns.support.annotation.Login;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
@@ -16,6 +17,7 @@ import java.security.Principal;
 public class CommentController {
     private final CommentService commentService;
 
+    @Login
     @PostMapping("/{postId}/comments")
     public ResultResponse<CommentDetailResponse> create(@ApiIgnore Principal principal, @PathVariable Long postId, @RequestBody CommentRequest commentRequest) {
         return ResultResponse.success(commentService.create(postId, principal.getName(), commentRequest));
