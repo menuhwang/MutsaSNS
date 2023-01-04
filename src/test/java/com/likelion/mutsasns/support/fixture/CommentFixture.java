1162e305cc72f5bbc3a097342cb0c626e5ba120e
menuHwang <hmin0206@naver.com>
2023-01-04 11:43:54 +0900

    [Test] 댓글 수정 기능 테스트

--- src/test/java/com/likelion/mutsasns/support/fixture/CommentFixture.java
+++ src/test/java/com/likelion/mutsasns/support/fixture/CommentFixture.java
@@ -6,15 +6,31 @@ import com.likelion.mutsasns.domain.user.User;
 import com.likelion.mutsasns.dto.comment.CommentDetailResponse;
 import com.likelion.mutsasns.dto.comment.CommentRequest;
 
+import static com.likelion.mutsasns.support.fixture.PostFixture.POST;
+import static com.likelion.mutsasns.support.fixture.UserFixture.USER;
+
 public enum CommentFixture {
-    COMMENT(1L, "content");
+    COMMENT(1L, "content", USER.init(), POST.init());
 
     private final Long id;
     private final String comment;
+    private final User user;
+    private final Post post;
 
-    CommentFixture(Long id, String comment) {
+    CommentFixture(Long id, String comment, User user, Post post) {
         this.id = id;
         this.comment = comment;
+        this.user = user;
+        this.post = post;
+    }
+
+    public Comment init() {
+        return Comment.builder()
+                .id(id)
+                .comment(comment)
+                .user(user)
+                .post(post)
+                .build();
     }
 
     public Comment init(User user, Post post) {
@@ -39,12 +55,25 @@ public enum CommentFixture {
         return new CommentRequest(comment);
     }
 
-    public CommentDetailResponse response(User user, Post post) {
+    public CommentDetailResponse response() {
         return CommentDetailResponse.builder()
                 .id(id)
                 .comment(comment)
                 .userName(user.getUsername())
                 .postId(post.getId())
+                .build();
+    }
+
+    public CommentRequest updateRequest() {
+        return new CommentRequest("update-" + comment);
+    }
+
+    public Comment update(CommentRequest update) {
+        return Comment.builder()
+                .id(id)
+                .comment(update.getComment())
+                .user(user)
+                .post(post)
                 .build();
     }
 }
--- src/test/java/com/likelion/mutsasns/support/fixture/PostFixture.java
+++ src/test/java/com/likelion/mutsasns/support/fixture/PostFixture.java
@@ -4,17 +4,30 @@ import com.likelion.mutsasns.domain.post.Post;
 import com.likelion.mutsasns.domain.user.User;
 import com.likelion.mutsasns.dto.post.PostRequest;
 
+import static com.likelion.mutsasns.support.fixture.UserFixture.USER;
+
 public enum PostFixture {
-    POST(1L, "title", "body");
+    POST(1L, "title", "body", USER.init());
 
     private final Long id;
     private final String title;
     private final String body;
+    private final User user;
 
-    PostFixture(Long id, String title, String body) {
+    PostFixture(Long id, String title, String body, User user) {
         this.id = id;
         this.title = title;
         this.body = body;
+        this.user = user;
+    }
+
+    public Post init() {
+        return Post.builder()
+                .id(id)
+                .title(title)
+                .body(body)
+                .user(user)
+                .build();
     }
 
     public Post init(User user) {
