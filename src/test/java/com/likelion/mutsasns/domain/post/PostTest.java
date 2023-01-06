b6813fc91741e9f8cf852dce7527e3a6d7088ffd
menuHwang <hmin0206@naver.com>
2023-01-06 16:02:21 +0900

    [Test] 동시성 문제 테스트

--- src/test/java/com/likelion/mutsasns/domain/post/PostTest.java
+++ src/test/java/com/likelion/mutsasns/domain/post/PostTest.java
@@ -3,6 +3,10 @@ package com.likelion.mutsasns.domain.post;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static com.likelion.mutsasns.support.fixture.PostFixture.POST;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -37,5 +41,31 @@ class PostTest {
         post.unlikes();
 
         assertEquals(0, post.getLikes());
+    }
+
+    @Test
+    @DisplayName("좋아요 : 동시성 테스트")
+    void like_multi_thread() throws InterruptedException {
+        /* // 동시성 테스트용 비즈니스 로직 Post.likes()
+        try {
+            int temp = likes;
+            Thread.sleep(100);
+            likes = temp + 1;
+        }catch (InterruptedException e) {
+        }
+        */
+        int numberOfThreads = 10;
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        CountDownLatch latch = new CountDownLatch(numberOfThreads);
+        Post post = POST.init();
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            service.execute(() -> {
+                post.likes();
+                latch.countDown();
+            });
+        }
+        latch.await();
+        assertEquals(numberOfThreads, post.getLikes());
     }
 }
