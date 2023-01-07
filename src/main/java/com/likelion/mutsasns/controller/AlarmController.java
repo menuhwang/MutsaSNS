00b1a2183da20e8d35a6c5e437a808d23cc5aafc
menuHwang <hmin0206@naver.com>
2023-01-07 16:56:08 +0900

    [Fix] Swagger Authentication 헤더 설정 추가

--- src/main/java/com/likelion/mutsasns/controller/AlarmController.java
+++ src/main/java/com/likelion/mutsasns/controller/AlarmController.java
@@ -3,6 +3,7 @@ package com.likelion.mutsasns.controller;
 import com.likelion.mutsasns.dto.ResultResponse;
 import com.likelion.mutsasns.dto.alarm.AlarmResponse;
 import com.likelion.mutsasns.service.AlarmService;
+import com.likelion.mutsasns.support.annotation.Login;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,6 +20,7 @@ import java.security.Principal;
 public class AlarmController {
     private final AlarmService alarmService;
 
+    @Login
     @GetMapping("")
     public ResultResponse<Page<AlarmResponse>> findMyAlarms(@ApiIgnore Principal principal, Pageable pageable) {
         Page<AlarmResponse> alarmResponses = alarmService.findMyAlarms(principal.getName(), pageable);
